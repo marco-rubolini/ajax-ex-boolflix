35dd72caf652e9647f6c033a51a32bb598ae1d51
Marco <rubolini.marco@gmail.com>
2020-05-22 15:49:22 +0200

    Versione rapida html-css e prova evento js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+// Predisporre quindi un layout molto semplice con una barra di ricerca e un pulsante:
 
+
+
+
+// al click sul pulsante fare partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
+// Ciclare i risultati e per ogni film restituito, stamparne in pagina:
+// titolo
+// titolo originale
+// lingua
+// voto
+
+
+// Intercetto il click sul pulsante di ricerca
+$('#search-button').click(function(){
+    alert('ciao');
+})
