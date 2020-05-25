90d1c16c80ae5e7d708e0ec00b32491c60468ce6
Marco <rubolini.marco@gmail.com>
2020-05-25 16:22:32 +0200

    Milestone 2 prima parte

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ $(document).ready(function(){
     var source   = $("#template-film").html();
     var template = Handlebars.compile(source);
 
+
 // Intercetto il click sul pulsante di ricerca
 $('#search-button').click(function(){
     ricercaFilm();
 })
 
-$('#text-search').keypress(function(event){
+$('#text-search').keyup(function(event){
     if (event.which == 13) {
         ricercaFilm();
     }
@@ -17,71 +18,71 @@ $('#text-search').keypress(function(event){
 
  function ricercaFilm (){
      // Recupero la query di ricerca inserita dall'utente
-     var searchUser = $('#text-search').val();
-     // ripulisco la ricerca precedente
-     $('ul').remove();
-     // all'evento associato far partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
-     $.ajax({
-         'url': 'https://api.themoviedb.org/3/search/movie',
-         'method': 'GET',
-         'data': {
-             'api_key': '293167d93b32235a7163c8c41e8172d5',
-             'query': searchUser,
-             'language': 'it-IT'
-         },
-         'success': function(risposta){
-             console.log(risposta.results);
-             var elencoFilm = risposta.results;
-             console.log(elencoFilm);
+     var searchUser = $('#text-search').val().trim();
+     if (searchUser.length > 1) {
+         resetRisultati();
+         // all'evento associato far partire una chiamata ajax a tmdb per recuperare i film che corrispondo alla query di ricerca inserita dall'utente.
+         $.ajax({
+             'url': 'https://api.themoviedb.org/3/search/movie',
+             'method': 'GET',
+             'data': {
+                 'api_key': '293167d93b32235a7163c8c41e8172d5',
+                 'query': searchUser,
+                 'language': 'it-IT'
+             },
+             'success': function(risposta){
 
-             // Ciclare i risultati
-             for (var i = 0; i < elencoFilm.length; i++) {
-                 var film = elencoFilm[i];
-                 console.log(elencoFilm[i]);
-                 console.log(elencoFilm[i].title);
-                 console.log(film.title);
-                 console.log(elencoFilm[i].original_title);
-                 console.log(elencoFilm[i].original_language);
-                 console.log(elencoFilm[i].vote_average);
+                 var elencoFilm = risposta.results;
 
-                 // per ogni film restituito, stamparne in pagina:
-                 // titolo
-                 var titolo = elencoFilm[i].title;
-                 // titolo originale
-                 var titoloOriginale = elencoFilm[i].original_title;
-                 // lingua
-                 var lingua = elencoFilm[i].original_language;
-                 // voto
-                 var voto = elencoFilm[i].vote_average;
-
-                 var context = {
-                     titolo_film: titolo,
-                     titolo_originale: titoloOriginale,
-                     lingua: lingua,
-                     recensioni: voto
-                 };
-
-                 var html = template(context);
-
-                 $('.elencofilm').append(html);
-                 // // per ogni film restituito, stamparne in pagina:
-                 // // titolo
-                 // var titolo = 'Titolo ' + elencoFilm[i].title;
-                 // // titolo originale
-                 // var titoloOriginale = 'Titolo originale ' + elencoFilm[i].original_title;
-                 // // lingua
-                 // var lingua = 'Lingua originale ' + elencoFilm[i].original_language;
-                 // // voto
-                 // var voto = 'Voto ' + elencoFilm[i].vote_average;
-                 // stampo in pagina
-                 // var stamparisultato = $('.container').append('<ul><li>' + titolo + '</li><li>' + titoloOriginale + '</li><li>' + lingua + '</li><li>' + voto + '</li>');
+                 // Ciclare i risultati
+                 for (var i = 0; i < elencoFilm.length; i++) {
+                     var film = elencoFilm[i];
+                     console.log(film);
+                     disegna_card(film);
+                 }
+             },
+             'error': function(){
+                 console.log('errore');
              }
-         },
-         'error': function(){
-             console.log('errore');
-         }
 
-     })
+         })
+     } else {
+         // l'utente ha digitato meno di 2 caratteri
+         alert('devi digitare almeno 2 caratteri');
+     }
+
  }
+
+ function resetRisultati (){
+     // ripulisco la ricerca precedente
+     $('.elencofilm .cardfilm').empty();
+     // Ripulisco la ricerca inserita dall'utente nell'input
+     $('#text-search').val('');
+ }
+
+function disegna_card(dati){
+
+    var vote = parseInt(dati.vote_average)/2
+
+    var context = {
+        titolo_film: dati.title,
+        titolo_originale: dati.original_title,
+        lingua: dati.original_language,
+        recensioni : disegnaStelle(vote)
+    };
+
+    var html = template(context);
+
+    $('.elencofilm').append(html);
+}
+
+function disegnaStelle (num){
+    var stars = '';
+    for (var i = 0; i < num; i++) {
+        stars += '<i class="fas fa-star"></i>';
+    }
+    return stars
+}
+
 
 })
